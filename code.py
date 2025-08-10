808978c99fc650451588d53be2414a8c35068a84
sam blenny <68084116+samblenny@users.noreply.github.com>
2025-08-10 01:21:57 +0000

    test for usb.core.find() + unplugging device
    find() may break if you unplug a USB device while its generator is
    active.

--- code.py
+++ code.py
@@ -1,8 +1,15 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: Copyright 2025 Sam Blenny
 #
-import test_1
 
 # Test 1: Connect to gamepad and poll rapidly for input events. Combining this
 # with trying to write a file to CIRCUITPY may crash the board.
-test_1.run()
+if False:
+    import test_1
+    test_1.run()
+
+# Test 2: Repeatedly call usb.core.find() and check for any changes to the
+# device descriptor details of the devices it finds.
+if True:
+    import test_2
+    test_2.run()
